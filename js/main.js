f3384c1e9f162749bd095c2c011131feecf64b3e
Jesus <libertad28j2024@gmail.com>
2024-11-02 10:39:07 -0300

    termianndo trabajo de lm trabajo en servicios y carreras de youtube

--- js/main.js
+++ js/main.js
@@ -86,3 +86,34 @@ function efectoHabilidades() {
 }
 
 
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show')
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-card', {delay: 500});
+ScrollReveal().reveal('.card-banner-one', {delay: 500});
+ScrollReveal().reveal('.cards-banner-two', {delay: 500});
+
+
+// scripts de steelfira9239
+
+
+function scrollLeft() {
+    document.getElementById('videoSection').scrollBy({
+        top: 0,
+        left: -300,
+        behavior: 'smooth'
+    });
+}
+
+function scrollRight() {
+    document.getElementById('videoSection').scrollBy({
+        top: 0,
+        left: 300,
+        behavior: 'smooth'
+    });
+}
+
+
+
